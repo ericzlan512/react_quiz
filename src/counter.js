a2235476d0116631dabb9211d0781094705ae277
zlan <lanzhuoqin@gmail.com>
2020-07-07 20:45:34 -0400

    setup starter.

---
+++ src/counter.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+    render() {
+        return (
+            <div className={'counter_text'}>
+                <span className="counter counter_text">120</span>
+            </div>
+        );
+    }
+}
+
+export default Counter;
